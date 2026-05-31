0e2429f88602ee4fa01af75250b9a98228e58d45
song682 <songpengjie2007@163.com>
2026-05-31 12:39:40 +0800

    😺🩹🔢

--- src/main/java/decok/dfcdvadstf/catframe/Tags.java
+++ src/main/java/decok/dfcdvadstf/catframe/Tags.java
@@ -2,5 +2,5 @@ package decok.dfcdvadstf.catframe;
 public class Tags {
     public static final String NAME = "CatFrame";
     public static final String MODID = "dfdvdsfsAPI";
-    public static final String VERSION = "0.0.5-debug";
+    public static final String VERSION = "0.0.5";
 }

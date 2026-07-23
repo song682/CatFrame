aa6268ddbb561a88fa8f27cde8bcee741b5883f9
song682 <songpengjie2007@163.com>
2026-07-23 15:38:56 +0800

    upgrade

--- src/main/java/decok/dfcdvadstf/catframe/Tags.java
+++ src/main/java/decok/dfcdvadstf/catframe/Tags.java
@@ -3,5 +3,5 @@ package decok.dfcdvadstf.catframe;
 public class Tags {
     public static final String NAME = "CatFrame";
     public static final String MODID = "catframe";
-    public static final String VERSION = "0.5.1-hotfix";
+    public static final String VERSION = "0.5.2";
 }

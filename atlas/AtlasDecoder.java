9d12d8e70642feb3076706826b91012a173b60f9
song682 <songpengjie2007@163.com>
2026-08-13 21:06:23 +0800

    😺❌➖
    delete the bars of those things

--- atlas/AtlasDecoder.java
+++
@@ -1,4 +0,0 @@
-package decok.dfcdvadstf.catframe.resources.atlas;
-
-public class AtlasDecoder {
-}
---
+++ src/main/java/decok/dfcdvadstf/catframe/resources/atlas/AtlasDecoder.java
@@ -0,0 +1,12 @@
+package decok.dfcdvadstf.catframe.resources.atlas;
+
+import com.google.gson.Gson;
+
+public class AtlasDecoder {
+
+    private static Gson json =  new Gson();
+
+    private AtlasDecoder() {}
+
+    
+}
